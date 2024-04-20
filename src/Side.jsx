592f1111f3b35501e868879d8fc24c87c3e0014c
Shobande James Oluwaseun <shobandejames@gmail.com>
2024-04-20 15:12:07 +0100

    fixed hamburger button not clicking on mobile after deployent

--- src/Side.jsx
+++ src/Side.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { Disclosure } from "@headlessui/react";
+import { GrFormClose } from "react-icons/gr";
 import {
   MdOutlineSpaceDashboard,
   MdOutlineAnalytics,
@@ -17,18 +17,22 @@ import { IoMoon } from "react-icons/io5";
 import { IoSunny } from "react-icons/io5";
 
 function SideNavbar({darkModeHandler, dark}) {
+  const [modalOpen, setModalOpen] = useState(false)
+  const test = () => {
+    setModalOpen(!modalOpen)
+  }
   return (
     <div className="helvetica">
-      <Disclosure as="div">
-        <Disclosure.Button className="absolute top-6 right-4 inline-flex items-center peer justify-start rounded-md p-2 text-gray-800 hover:bg-gray-900">
-          <GiHamburgerMenu
-            className="block md:hidden h-6 w-6 z-50"
-            aria-hidden="true"
-          />
-        </Disclosure.Button>
-        <div className="overflow-y-auto p-6 w-1/2 h-screen z-20 fixed top-0 -left-96 lg:left-0 lg:w-1/6 peer-focus:left-0 peer:transition ease-out delay-250 duration-300 bg-white dark:bg-blue-900 border-r-2 border-gray-100">
+       <div onClick={test} className="absolute top-6 right-4 inline-flex items-center peer justify-start rounded-md p-2 text-gray-800 hover:bg-gray-900">
+        {modalOpen?
+        <GrFormClose className="block lg:hidden h-6 w-6 z-50" aria-hidden="true"/> :
+          <GiHamburgerMenu className="block lg:hidden h-6 w-6 z-50" aria-hidden="true"/>
+        }
+        </div>
+      <div className= {` ${modalOpen ? '' : 'hidden lg:block'}`}>
+        <div onClick= {()=> setModalOpen(!modalOpen)} className="overflow-y-auto p-6 w-1/2 h-screen z-20 fixed top-0 lg:left-0 md:w-1/3 lg:w-1/6 peer-focus:left-0 peer:transition ease-out delay-250 duration-300 bg-white dark:bg-blue-900 border-r-2 border-gray-100">
           <div className="flex flex-col justify-start item-center">
-            <h1 className="text-2xl cursor-pointer font-bold text-blue-500 border-b border-gray-100 pb-4 w-full sora">
+            <h1 className="text-1xl lg:text-2xl cursor-pointer font-bold text-blue-500 border-b border-gray-100 pb-4 w-full sora">
               Estate Helper
             </h1>
             <div className=" my-3 border-b border-gray-100 pb-4">
@@ -108,7 +112,7 @@ function SideNavbar({darkModeHandler, dark}) {
    
           </div>
         </div>
-      </Disclosure>
+      </div>
     </div>
   );
 }
