c91395c6386f356c25ffb338f6858da6f93dd569
Shobande James Oluwaseun <shobandejames@gmail.com>
2024-06-18 15:35:36 +0100

    ui for add product modal

--- src/components/product/AddProductModal.jsx
+++ src/components/product/AddProductModal.jsx
@@ -5,13 +5,16 @@ import { uploadCloudinary } from "../../app/upload";
 const AddProductModal = ({ isOpen, closeModal }) => {
     const [imageUrls, setImageUrls] = useState([])
     const [formData, setFormData] = useState({
-        surname: '',
-        firstname: '', 
+        name: '',
+        location: '',
+        description: ''
     });
 
     const [errors, setErrors] = useState({
-        surname: '',
-        firstname: '', 
+        name: '',
+        location: '',
+        images: '',
+        description: ''
     });
 
     const handleChange = (e) => {
@@ -28,18 +31,25 @@ const AddProductModal = ({ isOpen, closeModal }) => {
 
     const validate = () => {
         const newErrors = {};
-        if (!formData.surname) {
-            newErrors.surname = 'Surname is required';
+        if (!formData.name) {
+            newErrors.name = 'Name is required';
         }
-        if (!formData.firstname) {
-            newErrors.firstname = 'First Name is required';
+        if (!formData.location) {
+            newErrors.location = 'Location is required';
         }
+        if (!formData.description) {
+            newErrors.description = "Description is required"
+        }
+        if (imageUrls.length == 0) {
+            newErrors.images = "Please select at least one image"
+        }
+        console.log(newErrors)
         return newErrors;
     };
 
     const uploadImages = async (selectedFiles) => {
         let arr = []
-        if(selectedFiles) {
+        if (selectedFiles) {
             for (let i = 0; i < selectedFiles?.length; i++) {
                 const data = await uploadCloudinary(selectedFiles[i]);
                 arr.push(data);
@@ -47,6 +57,43 @@ const AddProductModal = ({ isOpen, closeModal }) => {
         }
         return arr
     }
+
+    const handleFileChange = async (selectedFiles) => {
+        setErrors({ ...errors, images: '' });
+        const validImageTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        const maxSize = 2 * 1024 * 1024; // 2MB in bytes
+        if (selectedFiles.length > 0) {
+            for (let i = 0; i < selectedFiles.length; i++) {
+                if (!validImageTypes.includes(selectedFiles[i].type)) {
+                    setErrors({ ...errors, images: 'Please select valid image files (jpeg, png, gif).' });
+                    return;
+                }
+                if (selectedFiles[i].size > maxSize) {
+                    setErrors({ ...errors, images: 'File size must be less than 2MB.' });
+                    return;
+                }
+            }
+        }
+        // if(selectedFiles) {
+        //     const uploadUrls = await uploadImages(selectedFiles)
+        //     setImageUrls(uploadUrls)
+        // } 
+        else {
+            setErrors({ ...errors, images: 'Please select at least one image' });
+        }
+    }
+
+    const clearAllKeys = (arr) => {
+        return arr.map(item => {
+            const newItem = {};
+            for (const key in item) {
+                if (item.hasOwnProperty(key)) {
+                    newItem[key] = '';
+                }
+            }
+            return newItem;
+        });
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -60,46 +107,120 @@ const AddProductModal = ({ isOpen, closeModal }) => {
             // Process form data
             console.log(submittedData);
             // Reset form
-            setFormData({
-                surname: '',
-                firstname: '', 
-            });
+            const updatedFormData = clearAllKeys(formData);
+            setFormData(updatedFormData);
+            // setFormData({
+            //     name: '',
+            //     location: '', 
+            //     description: ''
+            // });
         }
     };
 
     return (
         <Modal isOpen={isOpen} onClose={closeModal}>
             <h2 className="text-xl font-bold py-2">Add Product</h2>
-            <hr />
+            <hr className="pb-2" />
 
             <form onSubmit={handleSubmit} >
 
                 <div className='flex justify-between'>
                     <div className="py-2 pe-2 basis-1/2">
-                        <label htmlFor="surname" className="block text-sm text-gray-700">Surname</label>
+                        <label htmlFor="name" className="block text-sm text-gray-700">Name</label>
                         <input
                             type="text"
-                            id="surname"
-                            name="surname"
-                            value={formData.surname}
+                            id="name"
+                            name="name"
+                            value={formData.name}
                             onChange={handleChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
                         />
-                        {errors.surname && <p className="text-red-500 text-xs mt-1">{errors.surname}</p>}
+                        {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name}</p>}
                     </div>
 
                     <div className="py-2 ps-2 basis-1/2">
-                        <label htmlFor="firstname" className="block text-sm text-gray-700">First Name</label>
+                        <label htmlFor="location" className="block text-sm text-gray-700">Location</label>
                         <input
                             type="text"
-                            id="firstname"
-                            name="firstname"
-                            value={formData.firstname}
+                            id="location"
+                            name="location"
+                            value={formData.location}
                             onChange={handleChange}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
                         />
-                        {errors.firstname && <p className="text-red-500 text-xs mt-1">{errors.firstname}</p>}
+                        {errors.location && <p className="text-red-500 text-xs mt-1">{errors.location}</p>}
                     </div>
+                </div>
+
+                <div className='flex justify-between'>
+                    <div className="py-2 pe-2 basis-1/4">
+                        <label htmlFor="price" className="block text-sm text-gray-700">Price</label>
+                        <input
+                            type="text"
+                            id="price"
+                            name="price"
+                            value={formData.price}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.price && <p className="text-red-500 text-xs mt-1">{errors.price}</p>}
+                    </div>
+
+                    <div className="py-2 pe-2 basis-1/4">
+                        <label htmlFor="survey" className="block text-sm text-gray-700">Survey</label>
+                        <input
+                            type="text"
+                            id="survey"
+                            name="survey"
+                            value={formData.survey}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.survey && <p className="text-red-500 text-xs mt-1">{errors.survey}</p>}
+                    </div>
+
+                    <div className="py-2 ps-2 basis-1/4">
+                        <label htmlFor="development" className="block text-sm text-gray-700">Development</label>
+                        <input
+                            type="text"
+                            id="development"
+                            name="development"
+                            value={formData.development}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.development && <p className="text-red-500 text-xs mt-1">{errors.development}</p>}
+                    </div>
+
+                    <div className="py-2 ps-2 basis-1/4">
+                        <label htmlFor="unit" className="block text-sm text-gray-700">Unit</label>
+                        <select
+                            id="unit"
+                            name="unit"
+                            value={formData.unit}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-blue-300 rounded-md"
+                        >
+                            {/* <option value="">Select gender</option> */}
+                            <option  value="0">SquareMeter</option>
+                            <option value="1">Plot</option>
+                        </select>
+                        {errors.unit && <p className="text-red-500 text-xs mt-1">{errors.unit}</p>}
+                    </div>
+                </div>
+
+                <div className="py-2">
+                    <label htmlFor="description" className="block text-sm text-gray-700">Description</label>
+                    <textarea
+                        rows={5}
+                        type="text"
+                        id="description"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                    />
+                    {errors.description && <p className="text-red-500 text-xs mt-1">{errors.description}</p>}
                 </div>
 
                 <div className="py-2 ">
@@ -110,17 +231,13 @@ const AddProductModal = ({ isOpen, closeModal }) => {
                         name="images"
                         multiple
                         value={formData.images}
-                        onChange={async(event)=> {
+                        onChange={async (event) => {
                             const selectedFiles = event.currentTarget.files
-                            if(selectedFiles) {
-                                const uploadUrls = await uploadImages(selectedFiles)
-                                setImageUrls(uploadUrls)
-                                console.log(uploadUrls)
-                            }
+                            await handleFileChange(selectedFiles)
                         }}
                         className="mt-1 block w-full px-3 border border-gray-300 rounded-sm file:bg-blue-100 file:text-blue-700 file:rounded-full file:border-0 file:py-2 file:px-4 hover:file:bg-blue-200"
                     />
-                    {errors.firstname && <p className="text-red-500 text-xs mt-1">{errors.firstname}</p>}
+                    {errors.images && <p className="text-red-500 text-xs mt-1">{errors.images}</p>}
                 </div>
 
                 <div className="py-2 ">
